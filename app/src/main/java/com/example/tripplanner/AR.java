cb121af0bf6fe4dc416825dad3e1abfb89dc881e
gadi <gadi.fleischer@mail.huji.ac.il>
2021-09-17 17:06:02 +0300

    playing with AR

--- app/src/main/java/com/example/tripplanner/AR.java
+++ app/src/main/java/com/example/tripplanner/AR.java
@@ -1,32 +1,20 @@
 package com.example.tripplanner;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.os.Handler;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.ar.core.Anchor;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
+import com.google.ar.core.ArCoreApk;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 
 public class AR extends AppCompatActivity
 {
-    private static final String TAG = AR.class.getSimpleName();;
-    private static final double MIN_OPENGL_VERSION =4.0 ;
-    private Uri selectedObject;
 
     private ArFragment arFragment;
     private ModelRenderable modelRenderable;
@@ -35,32 +23,33 @@ public class AR extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ar_activity);
-
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-        setUpModel();
-        setUpPlane();
+        maybeEnableArButton();
     }
 
     private void setUpModel() {
         ModelRenderable.builder()
-                .setSource(this, R.raw.wolves)
+                .setSource(this, R.raw.earth2)
                 .build()
                 .thenAccept(renderable -> modelRenderable = renderable)
                 .exceptionally(throwable -> {
                     Toast.makeText(AR.this,"Model can't be Loaded", Toast.LENGTH_SHORT).show();
                     return null;
                 });
+//        ModelRenderable.builder()
+//                .setSource(this, Uri.parse("wolves.sfb"))
+//                .build()
+//                .thenAccept(renderable -> modelRenderable = renderable)
+//                .exceptionally(throwable -> {
+//                    Toast.makeText(AR.this,"Model can't be Loaded", Toast.LENGTH_SHORT).show();
+//                    return null;
+//                });
     }
-
     private void setUpPlane(){
-        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
-            @Override
-            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
-                Anchor anchor = hitResult.createAnchor();
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
-                createModel(anchorNode);
-            }
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            createModel(anchorNode);
         });
     }
 
@@ -71,20 +60,26 @@ public class AR extends AppCompatActivity
         node.select();
 
     }
-//    public static boolean checkIsSupportedDevice(final Activity activity)
-//    {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
-//        {
-//            Log.e(TAG, "Sceneform requires Android N or later");            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-//            activity.finish();
-//            return false;
-//        }          String openGlVersionString = ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE)).getDeviceConfigurationInfo()
-//            .getGlEsVersion();
-//        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-//            Log.e(TAG, "Sceneform requires OpenGL ES 4.0 later");             Toast.makeText(activity, "Sceneform requires OpenGL ES 4.0 or later", Toast.LENGTH_LONG).show();
-//            activity.finish();
-//            return false;
-//        }
-//        return true;
-//    }
+    void maybeEnableArButton() {
+        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
+        if (availability.isTransient()) {
+            // Continue to query availability at 5Hz while compatibility is checked in the background.
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    maybeEnableArButton();
+                }
+            }, 200);
+        }
+        if (availability.isSupported()) {
+            arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+            setUpModel();
+            setUpPlane();
+        } else { // The device is unsupported or unknown.
+            Toast toast = Toast.makeText(this,"your Device Cant Handle AR Feature", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+    }
+
 }
